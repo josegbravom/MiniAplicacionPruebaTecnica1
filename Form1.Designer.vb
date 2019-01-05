89a322c0a7f86732e43408105f39fcd4e6ac4524
josegbravom <josegbravom@gmail.com>
2019-01-05 14:43:22 -0300

    Initial commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,52 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMiniAppPdfRest
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.bIniciarGeneracion = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'bIniciarGeneracion
+        '
+        Me.bIniciarGeneracion.Location = New System.Drawing.Point(175, 44)
+        Me.bIniciarGeneracion.Name = "bIniciarGeneracion"
+        Me.bIniciarGeneracion.Size = New System.Drawing.Size(136, 37)
+        Me.bIniciarGeneracion.TabIndex = 0
+        Me.bIniciarGeneracion.Text = "Iniciar Generación"
+        Me.bIniciarGeneracion.UseVisualStyleBackColor = True
+        '
+        'frmMiniAppPdfRest
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(484, 126)
+        Me.Controls.Add(Me.bIniciarGeneracion)
+        Me.MaximizeBox = False
+        Me.Name = "frmMiniAppPdfRest"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Aplicación que genera PDF desde un REST"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents bIniciarGeneracion As System.Windows.Forms.Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,67 @@
+﻿Imports System.Net
+Imports System.IO
+Imports System.Web.Script.Serialization
+Imports iTextSharp
+Imports iTextSharp.text
+Imports iTextSharp.text.pdf
+
+
+
+Public Class frmMiniAppPdfRest
+
+    Private Sub bIniciarGeneracion_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles bIniciarGeneracion.Click
+
+        Dim Direccion As String
+        Dim RecibeREST As HttpWebRequest
+        Dim Respuesta As HttpWebResponse
+        Dim Leer As StreamReader
+        Dim ResultadoString As String
+        'para crear el pdf
+        Dim documentoPDF As New Document
+
+        Dim appPath As String = Application.StartupPath()
+        appPath &= "\ReportePdf.pdf"
+        PdfWriter.GetInstance(documentoPDF, New FileStream(appPath, FileMode.Create))
+        documentoPDF.Open()
+        
+
+
+        Direccion = "https://my-json-server.typicode.com/HaibuSolutions/prueba-tecnica-sf/user"
+        'Se obtiene el REST json
+        RecibeREST = DirectCast(WebRequest.Create(Direccion), HttpWebRequest)
+        'Metodo Get
+        Respuesta = DirectCast(RecibeREST.GetResponse(), HttpWebResponse)
+        'Obtengo el REST en una cadena
+        Leer = New IO.StreamReader(Respuesta.GetResponseStream())
+        ResultadoString = Leer.ReadToEnd()
+
+        Dim Campos As Object = New JavaScriptSerializer().Deserialize(Of List(Of Object))(ResultadoString)
+
+        For Each Campo As Object In Campos
+
+            'MsgBox(Campo("direccion")("calle").ToString)
+            documentoPDF.Add(New Paragraph(Campo("nombre"),
+                          FontFactory.GetFont(FontFactory.TIMES, 11,
+                              iTextSharp.text.Font.NORMAL)))
+
+        Next
+
+        documentoPDF.Close()
+        If System.IO.File.Exists(appPath) Then
+            If MsgBox("Texto convertido a fichero PDF correctamente " + _
+                   "¿desea abrir el fichero PDF resultante?",
+                   MsgBoxStyle.Question + MsgBoxStyle.YesNo) = MsgBoxResult.Yes Then
+                'Abrimos el fichero PDF con la aplicación asociada
+                System.Diagnostics.Process.Start(appPath)
+            End If
+        Else
+            MsgBox("El fichero PDF no se ha generado, " + _
+                   "compruebe que tiene permisos en la carpeta de destino.",
+                   MsgBoxStyle.Exclamation + MsgBoxStyle.OkOnly)
+        End If
+
+
+
+    End Sub
+    
+End Class
